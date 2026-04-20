5bce90d55e53fd02702d006fc3d8a78c04704e6f
felixmedinauth <a24311090@uthermosillo.edu.mx>
2026-04-19 21:32:35 -0700

    Corregir STATIC_ROOT para PythonAnywhere

--- facebook_integration/facebook_integration/settings.py
+++ facebook_integration/facebook_integration/settings.py
@@ -154,8 +154,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/home/examenu3Felixmedina/facebook/facebook_integration/staticfiles/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/examenu3Felixmedina/felixmedinafacebook/facebook_integration/staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
